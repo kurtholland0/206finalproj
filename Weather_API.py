e430a3039450d62e29cec9810f6eb8feca09b4fe
camnic <camnic@umich.edu>
2023-04-20 15:57:19 -0400

    docstrings

--- Weather_API.py
+++ Weather_API.py
@@ -5,12 +5,47 @@ from datetime import datetime
 import apikey
 
 def get_weather_data(lat, lon, dt):
+    """
+    Get historical weather data for a specific latitude, longitude, and date.
+
+    This function retrieves historical weather data from the OpenWeatherMap API
+    using the provided latitude, longitude, and Unix timestamp (date). The API key
+    from the 'apikey' module is used to authenticate the API request.
+
+    Args:
+    lat (float): The latitude coordinate of the location for which to fetch the weather data.
+    lon (float): The longitude coordinate of the location for which to fetch the weather data.
+    dt (int): The Unix timestamp (date) for which to fetch the weather data.
+
+    Returns:
+    dict: The weather data in JSON format, as returned by the OpenWeatherMap API.
+    """
     url = f'https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={dt}&appid={apikey.API_KEY}'
     response = requests.get(url)
     data = response.json()
     return data
 
 def store_weather_data(cur, conn, track_id, lat, lon, race_date):
+    """
+    Store weather data for a specific track and race date in the F1_Weather table.
+
+    This function first creates the F1_Weather table if it doesn't already exist. Then, it converts the given
+    race date to a Unix timestamp and retrieves the weather data using the get_weather_data function.
+    If valid temperature, humidity, and wind speed values are available, the function inserts the data into
+    the F1_Weather table. If any of the values are missing, the function prints an appropriate message.
+
+    Args:
+    cur (sqlite3.Cursor): An SQLite cursor object for executing SQL commands.
+    conn (sqlite3.Connection): An SQLite connection object for committing the transaction.
+    track_id (int): The unique identifier of the F1 track.
+    lat (float): The latitude coordinate of the location for which to fetch the weather data.
+    lon (float): The longitude coordinate of the location for which to fetch the weather data.
+    race_date (str): The race date in the format 'YYYY-MM-DD'.
+
+    Side Effects:
+    Inserts the weather data into the F1_Weather table if valid data is available. Prints messages
+    regarding the insertion status and any missing data.
+    """
     cur.execute("CREATE TABLE IF NOT EXISTS F1_Weather (track_id INTEGER UNIQUE PRIMARY KEY, race_date TEXT, temp FLOAT, humidity INTEGER, wind_speed FLOAT, FOREIGN KEY(track_id) REFERENCES F1_Track_Names(id))")
     
     unix_timestamp = int(datetime.strptime(race_date, '%Y-%m-%d').strftime('%s'))
@@ -35,6 +70,21 @@ def store_weather_data(cur, conn, track_id, lat, lon, race_date):
         print(f"No data available for track_id {track_id}, race_date {race_date}")
 
 def collect_weather_data(cur, conn):
+    """
+    Collect and store weather data for each F1 track on their corresponding race dates.
+
+    This function retrieves the track information (ID, latitude, and longitude) from the F1_Track_Names table
+    and the race dates from the F1_Times table. It then calls the store_weather_data function for each track
+    and its corresponding race date to fetch and store the historical weather data in the F1_Weather table.
+
+    Args:
+    cur (sqlite3.Cursor): An SQLite cursor object for executing SQL commands.
+    conn (sqlite3.Connection): An SQLite connection object for committing the transaction.
+
+    Side Effects:
+    Calls the store_weather_data function for each track and corresponding race date, which in turn
+    inserts the weather data into the F1_Weather table if valid data is available.
+    """
     cur.execute("SELECT id, lat, long FROM F1_Track_Names")
     tracks = cur.fetchall()
 
--- Weather_Calc_Viz.py
+++ Weather_Calc_Viz.py
@@ -3,6 +3,22 @@ import csv
 import matplotlib.pyplot as plt
 
 def calculations(cur):
+    """
+    Calculate the average temperature for each F1 track using data from the SQLite database.
+
+    This function calculates the average temperature for each track by querying the F1_Track_Names and
+    F1_Weather tables in the database. It first retrieves a list of track IDs and then, for each track ID,
+    fetches the track name and temperature values from the database. The average temperature is then
+    computed for each track, and the results are stored in a dictionary where the keys are track names
+    and the values are the corresponding average temperatures.
+
+    Args:
+    cur (sqlite3.Cursor): An SQLite cursor object for executing SQL commands.
+
+    Returns:
+    dict: A dictionary containing the average temperature for each track, where the keys are track
+    names and the values are the corresponding average temperatures.
+    """
     avg_temp_dic = {}
     cur.execute("SELECT id FROM F1_Track_Names")
     rows = cur.fetchall()
@@ -18,6 +34,22 @@ def calculations(cur):
     return avg_temp_dic
 
 def write_file(data):
+    """
+    Write the average temperature data for each F1 track to a CSV file.
+
+    This function takes a dictionary containing the average temperature for each F1 track and writes the
+    data to a CSV file named 'avg_temp_at_track.csv'. The first row of the CSV file contains the column
+    headers 'Track Name' and 'Average Temperature'. Subsequent rows contain the track name and the
+    corresponding average temperature.
+
+    Args:
+    data (dict): A dictionary containing the average temperature for each track, where the keys are
+    track names and the values are the corresponding average temperatures.
+
+    Side Effects:
+    Creates a new CSV file named 'avg_temp_at_track.csv' with the average temperature data for each
+    F1 track.
+    """
     with open('avg_temp_at_track.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['Track Name', 'Average Temperature'])
@@ -25,6 +57,21 @@ def write_file(data):
             writer.writerow([track_name, avg_temp])
 
 def make_visualization(data):
+    """
+    Create a horizontal bar chart to visualize the average temperature data for each F1 track.
+
+    This function takes a dictionary containing the average temperature for each F1 track and creates a
+    horizontal bar chart using Matplotlib to visualize the data. The chart has a dim grey background, and
+    the bars are colored forest green. The chart's title, axis labels, and tick labels are formatted using
+    the Georgia font.
+
+    Args:
+    data (dict): A dictionary containing the average temperature for each track, where the keys are
+    track names and the values are the corresponding average temperatures.
+
+    Side Effects:
+    Displays the created horizontal bar chart of the average temperature data for each F1 track.
+    """
     track_names = list(data.keys())
     avg_temps = list(data.values())
 
